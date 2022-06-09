ec5c4098894399480695752dadf3eda8f221e9e3
entechlog <38563714+entechlog@users.noreply.github.com>
2022-06-09 09:54:26 -0700

    changes to add try logic to catch non existing users and roles (#6)

--- terraform/modules/main.tf
+++ terraform/modules/main.tf
@@ -7,6 +7,15 @@ terraform {
   }
 }
 
+locals {
+  enable_resource_mapping = {
+    snowflake-dev = 0
+    snowflake-stg = 0
+    snowflake-prd = 1
+  }
+  enable_resource_flag = local.enable_resource_mapping[terraform.workspace]
+}
+
 terraform {
   required_providers {
     snowflake = {
@@ -30,6 +39,7 @@ provider "snowflake" {
 
 module "all_users" {
   source = "./user"
+  count  = local.enable_resource_flag
   user_map = {
     "entechlog_dbt_user" : { "first_name" = "datastage", "last_name" = "User", "email" = "${lower(var.env_code)}_entechlog_dbt_user@example.com", "default_warehouse" = "DATASTAGE_WH", "default_role" = "DATASTAGE_ROLE" },
     "entechlog_atlan_user" : { "first_name" = "atlan", "last_name" = "User", "email" = "${lower(var.env_code)}_entechlog_atlan_user@example.com" }
@@ -48,39 +58,37 @@ output "all_users" {
 
 module "entechlog_dbt_role" {
   source       = "./roles"
+  count        = local.enable_resource_flag
   role_name    = "ENTECHLOG_DBT_ROLE"
   role_comment = "Snowflake role used by dbt"
 
   roles = ["SYSADMIN"]
-  users = [
-    "${module.all_users.user.entechlog_dbt_user.name}"
-  ]
+  users = [try(module.all_users[0].user.entechlog_dbt_user.name, "")]
 }
 
 module "entechlog_atlan_role" {
   source       = "./roles"
+  count        = local.enable_resource_flag
   role_name    = "ENTECHLOG_ATLAN_ROLE"
   role_comment = "Snowflake role used by Atlan"
 
   roles = ["SYSADMIN"]
-  users = [
-    "${module.all_users.user.entechlog_atlan_user.name}"
-  ]
+  users = [try(module.all_users[0].user.entechlog_atlan_user.name, "")]
 }
 
 module "entechlog_kafka_role" {
   source       = "./roles"
+  count        = local.enable_resource_flag
   role_name    = "ENTECHLOG_KAFKA_ROLE"
   role_comment = "Snowflake role used by Kafka"
 
   roles = ["SYSADMIN"]
-  users = [
-    "${module.all_users.user.entechlog_kafka_user.name}"
-  ]
+  users = [try(module.all_users[0].user.entechlog_kafka_user.name, "")]
 }
 
 module "entechlog_analyst_role" {
   source       = "./roles"
+  count        = local.enable_resource_flag
   role_name    = "ENTECHLOG_ANALYST_ROLE"
   role_comment = "Snowflake role used by Analyst"
 
@@ -98,7 +106,7 @@ module "entechlog_dbt_wh_xs" {
   warehouse_size = "XSMALL"
   warehouse_grant_roles = {
     "OWNERSHIP" = [var.snowflake_role]
-    "USAGE"     = [module.entechlog_dbt_role.role.name]
+    "USAGE"     = [try(module.entechlog_dbt_role[0].role.name, "")]
   }
 }
 
@@ -114,25 +122,25 @@ module "entechlog_raw_db" {
 
   db_grant_roles = {
     "OWNERSHIP" = [var.snowflake_role]
-    "USAGE"     = [module.entechlog_dbt_role.role.name]
+    "USAGE"     = [try(module.entechlog_dbt_role[0].role.name, "")]
     "USAGE"     = ["SYSADMIN"]
   }
 
   schemas = ["FACEBOOK", "GOOGLE", "COMPLIANCE"]
   schema_grant = {
-    "FACEBOOK OWNERSHIP"    = { "roles" = [module.entechlog_dbt_role.role.name] },
-    "GOOGLE OWNERSHIP"      = { "roles" = [module.entechlog_dbt_role.role.name] },
-    "FACEBOOK USAGE"        = { "roles" = [module.entechlog_dbt_role.role.name, module.entechlog_atlan_role.role.name, module.entechlog_kafka_role.role.name] },
-    "GOOGLE USAGE"          = { "roles" = [module.entechlog_dbt_role.role.name, module.entechlog_atlan_role.role.name, module.entechlog_kafka_role.role.name] },
-    "FACEBOOK CREATE TABLE" = { "roles" = [module.entechlog_dbt_role.role.name] },
-    "FACEBOOK CREATE VIEW"  = { "roles" = [module.entechlog_dbt_role.role.name] },
-    "GOOGLE CREATE TABLE"   = { "roles" = [module.entechlog_dbt_role.role.name] },
-    "GOOGLE CREATE VIEW"    = { "roles" = [module.entechlog_dbt_role.role.name] }
+    "FACEBOOK OWNERSHIP"    = { "roles" = [try(module.entechlog_dbt_role[0].role.name, "")] },
+    "GOOGLE OWNERSHIP"      = { "roles" = [try(module.entechlog_dbt_role[0].role.name, "")] },
+    "FACEBOOK USAGE"        = { "roles" = [try(module.entechlog_dbt_role[0].role.name, ""), try(module.entechlog_atlan_role[0].role.name, ""), try(module.entechlog_kafka_role[0].role.name, "")] },
+    "GOOGLE USAGE"          = { "roles" = [try(module.entechlog_dbt_role[0].role.name, ""), try(module.entechlog_atlan_role[0].role.name, ""), try(module.entechlog_kafka_role[0].role.name, "")] },
+    "FACEBOOK CREATE TABLE" = { "roles" = [try(module.entechlog_dbt_role[0].role.name, "")] },
+    "FACEBOOK CREATE VIEW"  = { "roles" = [try(module.entechlog_dbt_role[0].role.name, "")] },
+    "GOOGLE CREATE TABLE"   = { "roles" = [try(module.entechlog_dbt_role[0].role.name, "")] },
+    "GOOGLE CREATE VIEW"    = { "roles" = [try(module.entechlog_dbt_role[0].role.name, "")] }
   }
 
   table_grant = {
-    "FACEBOOK SELECT" = { "roles" = [module.entechlog_atlan_role.role.name] },
-    "GOOGLE SELECT"   = { "roles" = [module.entechlog_atlan_role.role.name] }
+    "FACEBOOK SELECT" = { "roles" = [try(module.entechlog_atlan_role[0].role.name, "")] },
+    "GOOGLE SELECT"   = { "roles" = [try(module.entechlog_atlan_role[0].role.name, "")] }
   }
 
 }
@@ -152,7 +160,7 @@ module "mp_encrypt_email" {
 
   masking_grants = {
     "OWNERSHIP" = [var.snowflake_role]
-    "APPLY"     = [module.entechlog_dbt_role.role.name]
+    "APPLY"     = [try(module.entechlog_dbt_role[0].role.name, "")]
   }
 }
 
@@ -169,7 +177,7 @@ module "entechlog_str_s3_intg" {
   storage_allowed_locations = ["s3://entechlog-demo/kafka-snowpipe-demo/"]
   storage_blocked_locations = ["s3://entechlog-demo/secure/"]
   storage_aws_role_arn      = "arn:aws:iam::001234567890:role/myrole"
-  roles                     = [module.entechlog_dbt_role.role.name]
+  roles                     = [try(module.entechlog_dbt_role[0].role.name, "")]
 }
 
 // Output block starts here

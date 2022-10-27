204df8e3de6430062d0a8d10671b83d6bb650b38
Siva Nadesan <entechlog@gmail.com>
2022-10-26 19:22:43 -0700

    changes to recreate new module decouple aws and snowflake

--- snow-ext-function/terraform/snow_api_integration.tf
+++
@@ -1,7 +0,0 @@
-resource "snowflake_api_integration" "api_integration" {
-  name                 = "aws_integration"
-  api_provider         = "aws_api_gateway"
-  api_aws_role_arn     = aws_iam_role.snow_ext_function.arn
-  api_allowed_prefixes = ["${aws_api_gateway_deployment.lambda_proxy.invoke_url}/"]
-  enabled              = true
-}

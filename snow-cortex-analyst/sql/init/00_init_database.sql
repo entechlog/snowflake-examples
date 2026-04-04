baff3ba26ef158063280e917c4eaa7aa1222a502
Siva Nadesan <entechlog@gmail.com>
2026-04-03 19:57:00 -0700

    changes to fix cortex demo sqls

--- snow-cortex-analyst/sql/init/00_init_database.sql
+++ snow-cortex-analyst/sql/init/00_init_database.sql
@@ -13,6 +13,9 @@ SET RAW_DB_NAME = $ENV_CODE || '_' || $PROJ_CODE || '_RAW_DB';
 SET DW_DB_NAME = $ENV_CODE || '_' || $PROJ_CODE || '_DW_DB';
 SET DBT_WH_NAME = $ENV_CODE || '_' || $PROJ_CODE || '_DBT_WH_XS';
 SET CORTEX_WH_NAME = $ENV_CODE || '_' || $PROJ_CODE || '_CORTEX_WH_XS';
+SET SCHEMA_SEED = $RAW_DB_NAME || '.SEED';
+SET SCHEMA_OBT = $DW_DB_NAME || '.OBT';
+SET SCHEMA_SEMANTIC = $DW_DB_NAME || '.SEMANTIC';
 
 USE ROLE SYSADMIN;
 
@@ -28,13 +31,13 @@ CREATE DATABASE IF NOT EXISTS IDENTIFIER($DW_DB_NAME)
 -- ---------------------------------------------------------------------------
 -- Schemas
 -- ---------------------------------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS IDENTIFIER($RAW_DB_NAME || '.SEED')
+CREATE SCHEMA IF NOT EXISTS IDENTIFIER($SCHEMA_SEED)
   COMMENT = 'Seed data loaded by dbt';
 
-CREATE SCHEMA IF NOT EXISTS IDENTIFIER($DW_DB_NAME || '.OBT')
+CREATE SCHEMA IF NOT EXISTS IDENTIFIER($SCHEMA_OBT)
   COMMENT = 'One Big Table layer - pre-aggregated views';
 
-CREATE SCHEMA IF NOT EXISTS IDENTIFIER($DW_DB_NAME || '.SEMANTIC')
+CREATE SCHEMA IF NOT EXISTS IDENTIFIER($SCHEMA_SEMANTIC)
   COMMENT = 'Semantic views for Cortex Analyst';
 
 -- ---------------------------------------------------------------------------
